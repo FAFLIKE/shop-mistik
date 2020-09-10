a4ba31b3b1aa12e0ab7f5140f732fe16fa4aa7db
FAFlike <flikemaster2@gmail.com>
2020-09-10 18:12:53 +0400

    Куки

--- tovar/views.py
+++ tovar/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import modelGoods
 
 def mainPage(request):
-    a = modelGoods.objects.all()
-    context = {'products': a,}
-    return render(request, 'tovar/goods/listGoods.html', context)
+    goods = modelGoods.objects.all()
+    session = request.session.get('1')
+    if session == None:
+        request.session['1'] = 0
+        session = request.session.get('1')
+
+    session = request.session.get('1')
+    request.session['1'] = session+1
+    context = {'products': goods, 'session': session,}
+    return render(request, 'tovar/mainPage.html', context)
 
 def infoAllGoods(request):
-    a = modelGoods.objects.all()
-    context = {'products': a,}
+    goods = modelGoods.objects.all()
+    context = {'products': goods,}
     return render(request, 'tovar/goods/listGoods.html', context)
 
 def infoGoods(request, numberGoods):
-    a = modelGoods.objects.all()
-    context = {'products': a[numberGoods], 'numberGoods': numberGoods,}
+    goods = get_object_or_404(modelGoods, id=numberGoods)
+    context = {'products': goods, 'numberGoods': numberGoods,}
     return render(request, 'tovar/goods/vievGoods.html', context)
     
